02f98d7159e0c115df9dbb0fd2e74ce3dee7f8a3
Thomas Cleary <thomasclearydev@gmail.com>
2023-09-24 21:55:06 +0800

    type-level-typescript: Loops with Recursive Types | challenge 3

---
+++ type-level-typescript/6-loops-with-recursive-types/challenge-3.ts
@@ -0,0 +1,52 @@
+/**
+ * Type the `filterTable` function to take a Table,
+ * a list of column names, and to return a table that
+ * only contains columns with these names.
+ */
+namespace filterTable {
+  type Column = { name: string; values: unknown[] };
+
+  declare function filterTable<
+    // Infer `T` as a tuple containing columns:
+    T extends [Column, ...Column[]],
+    // Infer `N` as a union of string literal type:
+    N extends string
+  >(table: T, columnNames: N[]): FilterTable<T, N>;
+
+  type FilterTable<Table, NameUnion> = Table extends [infer First extends Column, ...infer Rest]
+    ? First["name"] extends NameUnion
+      ? [First, ...FilterTable<Rest, NameUnion>]
+      : FilterTable<Rest, NameUnion>
+    : [];
+
+  // Solution
+  // This is a "filter" loop!
+  type FilterTableSolution<Table, NameUnion> =
+    // 1. split the list:
+    Table extends [infer First, ...infer Rest]
+      ? // 2. check if `First.name` is assignable to `NameUnion`:
+        First extends { name: NameUnion }
+        ? // 3. keep the column if it is:
+          [First, ...FilterTable<Rest, NameUnion>]
+        : // 4. not if it isn't:
+          FilterTable<Rest, NameUnion>
+      : // if the list is empty, return an empty list:
+        [];
+
+  declare const userTable: [
+    { name: "firstName"; values: string[] },
+    { name: "lastName"; values: string[] },
+    { name: "age"; values: number[] }
+  ];
+
+  const res1 = filterTable(userTable, ["age"]);
+  type test1 = Expect<Equal<typeof res1, [{ name: "age"; values: number[] }]>>;
+
+  const res2 = filterTable(userTable, ["firstName", "lastName"]);
+  type test2 = Expect<
+    Equal<typeof res2, [{ name: "firstName"; values: string[] }, { name: "lastName"; values: string[] }]>
+  >;
+
+  const res3 = filterTable(userTable, []);
+  type test3 = Expect<Equal<typeof res3, []>>;
+}
