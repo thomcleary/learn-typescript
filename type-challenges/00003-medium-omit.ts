c50a3d1eec43f013d70acdb433e69e227c38a13e
Thomas Cleary <thomasclearydev@gmail.com>
2023-10-08 17:14:48 +0800

    type-challenges: 3 - Omit

---
+++ type-challenges/00003-medium-omit.ts
@@ -0,0 +1,34 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, "description">>>,
+  Expect<Equal<Expected2, MyOmit<Todo, "description" | "completed">>>
+];
+
+// @ts-expect-error
+type error = MyOmit<Todo, "description" | "invalid">;
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+  completed: boolean;
+}
+
+interface Expected2 {
+  title: string;
+}
+
+// ============= Your Code Here =============
+type MyOmit<T, K extends keyof T> = {
+  [Key in Exclude<keyof T, K>]: T[Key];
+};
+
+// ============= Popular Solution =============
+type MyOmitPopularSolution<T, K extends keyof T> = { [P in keyof T as P extends K ? never : P]: T[P] };
+// uses Key remapping: https://www.typescriptlang.org/docs/handbook/2/mapped-types.html#key-remapping-via-as
