2811cf4fb9ab2036f6c80010e0b5640f15b3596c
Thomas Cleary <thomasclearydev@gmail.com>
2023-11-25 11:14:06 +0800

    type-challenges: 191 - Append Argument

---
+++ type-challenges/00191-medium-append-argument.ts
@@ -0,0 +1,21 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type Case1 = AppendArgument<(a: number, b: string) => number, boolean>;
+type Result1 = (a: number, b: string, x: boolean) => number;
+
+type Case2 = AppendArgument<() => void, undefined>;
+type Result2 = (x: undefined) => void;
+
+type cases = [
+  Expect<Equal<Case1, Result1>>,
+  Expect<Equal<Case2, Result2>>,
+  // @ts-expect-error
+  AppendArgument<unknown, undefined>
+];
+
+// ============= Your Code Here =============
+type AppendArgument<Fn extends (...args: any[]) => any, A> = (...args: [...Parameters<Fn>, A]) => ReturnType<Fn>;
+
+// ============= Popular Solution =============
+type AppendArgumentSolution<Fn, A> = Fn extends (...args: infer R) => infer T ? (...args: [...R, A]) => T : never;
