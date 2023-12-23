7dc7acc4817862ec34d9078830326694c2cf50ff
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-23 11:39:52 +0800

    type-hero: Parameters

---
+++ type-hero/challenges/easy/parameters.ts
@@ -0,0 +1,17 @@
+// Answer
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer A) => any ? A : never;
+
+type Test = MyParameters<typeof foo>;
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const foo = (arg1: string, arg2: number): void => {};
+const bar = (arg1: boolean, arg2: { a: "A" }): void => {};
+const baz = (): void => {};
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: "A" }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>
+];
