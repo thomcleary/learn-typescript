dbf401da890dc7dae34d1afbad393460d2c3750a
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-23 11:43:21 +0800

    type-hero: Readonly

---
+++ type-hero/challenges/easy/readonly.ts
@@ -0,0 +1,18 @@
+// Answer
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K];
+};
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>];
+
+interface Todo1 {
+  title: string;
+  description: string;
+  completed: boolean;
+  meta: {
+    author: string;
+  };
+}
