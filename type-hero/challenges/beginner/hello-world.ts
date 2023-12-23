61f221121b428ead63f2f5b9c660cb301a0ad4ed
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-23 09:27:06 +0800

    type-hero: Generic Function Arguments

--- type-hero/challenges/beginner/hello-world.ts
+++ type-hero/challenges/beginner/hello-world.ts
@@ -1,6 +1,7 @@
+// Answer
+type HelloWorld = string;
+
+// Tests
 import type { Equal, Expect, NotAny } from "@type-challenges/utils";
 
 type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>];
-
-// Answer
-type HelloWorld = string;
