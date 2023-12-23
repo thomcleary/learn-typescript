39db99194990b81324e671a8acb7d8122209e77e
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-23 11:03:17 +0800

    type-hero: Awaited

---
+++ type-hero/challenges/easy/awaited.ts
@@ -0,0 +1,28 @@
+// Answer
+type MyAwaited<P extends Promise<unknown> | PromiseLike<any>> = P extends Promise<infer T>
+  ? T extends Promise<unknown>
+    ? MyAwaited<T>
+    : T
+  : P extends PromiseLike<infer R>
+  ? R
+  : never;
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+type Z = Promise<Promise<string | number>>;
+type Z1 = Promise<Promise<Promise<string | boolean>>>;
+type T = { then: (onfulfilled: (arg: number) => any) => any };
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<Z1>, string | boolean>>,
+  Expect<Equal<MyAwaited<T>, number>>
+];
+
+// @ts-expect-error
+type error = MyAwaited<number>;
