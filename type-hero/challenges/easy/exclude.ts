118eabae99881306fed49b91b85242da7e35d557
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-23 11:13:13 +0800

    type-hero: Exclude

---
+++ type-hero/challenges/easy/exclude.ts
@@ -0,0 +1,13 @@
+// Answer
+type MyExclude<T, K> = T extends K ? never : T;
+
+type Test = MyExclude<"a" | "b", "b">;
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, "b" | "c">>,
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, "c">>,
+  Expect<Equal<MyExclude<string | number | (() => void), Function>, string | number>>
+];
