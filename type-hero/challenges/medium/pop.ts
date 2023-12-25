e189e4f32103e841b4e4b474e973c3d985c0bedf
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-25 12:34:43 +0800

    type-hero: Pop

---
+++ type-hero/challenges/medium/pop.ts
@@ -0,0 +1,11 @@
+// Answer
+type Pop<T extends unknown[]> = T extends [...infer First, infer Last] ? First : T;
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
+  Expect<Equal<Pop<["a", "b", "c", "d"]>, ["a", "b", "c"]>>,
+  Expect<Equal<Pop<[]>, []>>
+];
