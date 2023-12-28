fd1cc67e9c784830f1f2023d730273a213ab45b9
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-28 11:55:53 +0800

    type-hero: Trim

---
+++ type-hero/challenges/medium/trim.ts
@@ -0,0 +1,18 @@
+// Answer
+type Whitespace = " " | "\n" | "\t";
+
+type Trim<S extends string> = S extends `${Whitespace}${infer Rest}` | `${infer Rest}${Whitespace}` ? Trim<Rest> : S;
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Trim<"str">, "str">>,
+  Expect<Equal<Trim<" str">, "str">>,
+  Expect<Equal<Trim<"     str">, "str">>,
+  Expect<Equal<Trim<"str   ">, "str">>,
+  Expect<Equal<Trim<"     str     ">, "str">>,
+  Expect<Equal<Trim<"   \n\t foo bar \t">, "foo bar">>,
+  Expect<Equal<Trim<"">, "">>,
+  Expect<Equal<Trim<" \n\t ">, "">>
+];
