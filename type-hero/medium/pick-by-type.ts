013334dd9992275535bead0052bd0bf326fc1726
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-16 08:10:50 +0800

    type-hero: PickByType

---
+++ type-hero/medium/pick-by-type.ts
@@ -0,0 +1,20 @@
+// Answer
+type PickByType<T, U> = {
+  [Key in keyof T as T[Key] extends U ? Key : never]: T[Key];
+};
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+interface Model {
+  name: string;
+  count: number;
+  isReadonly: boolean;
+  isEnable: boolean;
+}
+
+type cases = [
+  Expect<Equal<PickByType<Model, boolean>, { isReadonly: boolean; isEnable: boolean }>>,
+  Expect<Equal<PickByType<Model, string>, { name: string }>>,
+  Expect<Equal<PickByType<Model, number>, { count: number }>>
+];
