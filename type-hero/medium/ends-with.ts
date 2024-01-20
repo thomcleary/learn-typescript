99e5e98e5640a8db7da010bb1ba4be95baaedd38
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-20 15:27:10 +0800

    type-hero: EndsWith

---
+++ type-hero/medium/ends-with.ts
@@ -0,0 +1,14 @@
+// Answer
+type EndsWith<T extends string, U extends string> = T extends `${infer Head}${U}` ? true : false;
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<EndsWith<"abc", "bc">, true>>,
+  Expect<Equal<EndsWith<"abc", "abc">, true>>,
+  Expect<Equal<EndsWith<"abc", "d">, false>>,
+  Expect<Equal<EndsWith<"abc", "ac">, false>>,
+  Expect<Equal<EndsWith<"abc", "">, true>>,
+  Expect<Equal<EndsWith<"abc", " ">, false>>
+];
