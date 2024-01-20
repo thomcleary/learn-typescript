d7401c31f9ba2d9ee7fc2744ad50d43e5b993ad3
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-20 15:40:10 +0800

    type-hero: OmitByType

---
+++ type-hero/medium/omit-by-type.ts
@@ -0,0 +1,20 @@
+// Answer
+type OmitByType<T, U> = {
+  [Key in keyof T as T[Key] extends U ? never : Key]: T[Key];
+};
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+interface Model {
+  name: string;
+  count: number;
+  isReadonly: boolean;
+  isEnable: boolean;
+}
+
+type cases = [
+  Expect<Equal<OmitByType<Model, boolean>, { name: string; count: number }>>,
+  Expect<Equal<OmitByType<Model, string>, { count: number; isReadonly: boolean; isEnable: boolean }>>,
+  Expect<Equal<OmitByType<Model, number>, { name: string; isReadonly: boolean; isEnable: boolean }>>
+];
