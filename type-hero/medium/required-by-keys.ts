2de6fd379a4adc7654dbab095ce0e8779a27900d
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-20 15:35:06 +0800

    type-hero: RequiredByKeys

---
+++ type-hero/medium/required-by-keys.ts
@@ -0,0 +1,33 @@
+// Answer
+type RequiredByKeys<T, K extends keyof T = keyof T> = Prettify<Required<Pick<T, K>> & Omit<T, K>>;
+
+type Prettify<T> = { [Key in keyof T]: T[Key] };
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+interface User {
+  name?: string;
+  age?: number;
+  address?: string;
+}
+
+interface UserRequiredName {
+  name: string;
+  age?: number;
+  address?: string;
+}
+
+interface UserRequiredNameAndAge {
+  name: string;
+  age: number;
+  address?: string;
+}
+
+type cases = [
+  Expect<Equal<RequiredByKeys<User, "name">, UserRequiredName>>,
+  Expect<Equal<RequiredByKeys<User, "name" | "age">, UserRequiredNameAndAge>>,
+  Expect<Equal<RequiredByKeys<User>, Required<User>>>,
+  // @ts-expect-error
+  Expect<Equal<RequiredByKeys<User, "name" | "unknown">, UserRequiredName>>
+];
