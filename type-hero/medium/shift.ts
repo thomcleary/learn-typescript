eb3b18ddbcd857a3c09ef31c32c6f7b23a41ab7c
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-20 15:54:02 +0800

    type-hero: Shift

---
+++ type-hero/medium/shift.ts
@@ -0,0 +1,14 @@
+// Answer
+type Shift<T extends unknown[]> = T extends [infer First, ...infer Rest] ? Rest : T;
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  // @ts-expect-error
+  Shift<unknown>,
+  Expect<Equal<Shift<[]>, []>>,
+  Expect<Equal<Shift<[1]>, []>>,
+  Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
+  Expect<Equal<Shift<["a", "b", "c", "d"]>, ["b", "c", "d"]>>
+];
