663d52501660e947b89ebfcb69e94d5ffcf0ae7b
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-22 08:14:38 +0800

    type-hero: IsTuple

---
+++ type-hero/medium/is-tuple.ts
@@ -0,0 +1,14 @@
+// Answer
+type IsTuple<T> = [T] extends [never] ? false : T extends readonly [infer F, ...infer R] | [] ? true : false;
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<IsTuple<[]>, true>>,
+  Expect<Equal<IsTuple<[number]>, true>>,
+  Expect<Equal<IsTuple<readonly [1]>, true>>,
+  Expect<Equal<IsTuple<{ length: 1 }>, false>>,
+  Expect<Equal<IsTuple<number[]>, false>>,
+  Expect<Equal<IsTuple<never>, false>>
+];
