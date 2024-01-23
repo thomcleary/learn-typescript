7dce088aa37c44d6410bc43b1c1a4eb4120f5840
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-23 08:20:02 +0800

    type-hero: Trim Right

---
+++ type-hero/medium/trim-right.ts
@@ -0,0 +1,15 @@
+// Answer
+type TrimRight<S extends string> = S extends `${infer Left}${" " | "\n" | "\t"}` ? TrimRight<Left> : S;
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<TrimRight<"str">, "str">>,
+  Expect<Equal<TrimRight<"str ">, "str">>,
+  Expect<Equal<TrimRight<"str     ">, "str">>,
+  Expect<Equal<TrimRight<"     str     ">, "     str">>,
+  Expect<Equal<TrimRight<"   foo bar  \n\t ">, "   foo bar">>,
+  Expect<Equal<TrimRight<"">, "">>,
+  Expect<Equal<TrimRight<"\n\t ">, "">>
+];
