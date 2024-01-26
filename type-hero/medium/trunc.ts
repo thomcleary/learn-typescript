cb811c62e2a5f7ed62f00a96f70eed2d35d9130c
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-26 18:42:48 +0800

    type-hero: Trunc

---
+++ type-hero/medium/trunc.ts
@@ -0,0 +1,17 @@
+// Answer
+type Trunc<T extends number | string> = `${T}` extends `${infer L}.${infer R}` ? (L extends "" ? "0" : L) : `${T}`;
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Trunc<0.1>, "0">>,
+  Expect<Equal<Trunc<0.2>, "0">>,
+  Expect<Equal<Trunc<1.234>, "1">>,
+  Expect<Equal<Trunc<12.345>, "12">>,
+  Expect<Equal<Trunc<-5.1>, "-5">>,
+  Expect<Equal<Trunc<".3">, "0">>,
+  Expect<Equal<Trunc<"1.234">, "1">>,
+  Expect<Equal<Trunc<"-10.234">, "-10">>,
+  Expect<Equal<Trunc<10>, "10">>
+];
