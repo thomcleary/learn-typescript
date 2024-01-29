346d77ea131839fb5754cebe2a5a5293e3f7e790
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-29 10:11:37 +0800

    type-hero: MapTypes

---
+++ type-hero/medium/map-types.ts
@@ -0,0 +1,38 @@
+// Answer
+type MapTypes<T, R extends { mapFrom: unknown; mapTo: unknown }> = {
+  [Key in keyof T]: T[Key] extends R["mapFrom"] ? Extract<R, { mapFrom: T[Key] }>["mapTo"] : T[Key];
+};
+
+// Tests
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<MapTypes<{ stringToArray: string }, { mapFrom: string; mapTo: [] }>, { stringToArray: [] }>>,
+  Expect<Equal<MapTypes<{ stringToNumber: string }, { mapFrom: string; mapTo: number }>, { stringToNumber: number }>>,
+  Expect<
+    Equal<
+      MapTypes<{ stringToNumber: string; skipParsingMe: boolean }, { mapFrom: string; mapTo: number }>,
+      { stringToNumber: number; skipParsingMe: boolean }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<{ date: string }, { mapFrom: string; mapTo: Date } | { mapFrom: string; mapTo: null }>,
+      { date: null | Date }
+    >
+  >,
+  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string; mapTo: Date | null }>, { date: null | Date }>>,
+  Expect<
+    Equal<
+      MapTypes<{ fields: Record<string, boolean> }, { mapFrom: Record<string, boolean>; mapTo: string[] }>,
+      { fields: string[] }
+    >
+  >,
+  Expect<Equal<MapTypes<{ name: string }, { mapFrom: boolean; mapTo: never }>, { name: string }>>,
+  Expect<
+    Equal<
+      MapTypes<{ name: string; date: Date }, { mapFrom: string; mapTo: boolean } | { mapFrom: Date; mapTo: string }>,
+      { name: boolean; date: string }
+    >
+  >
+];
